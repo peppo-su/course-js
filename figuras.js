111394a38044c75ff0ad4234426e1a653c09d46f
peppo-su <ramanujanmanwho@gmail.com>
2022-08-15 10:54:26 -0500

    last changes - add isosceles triangle

--- figuras.js
+++ figuras.js
@@ -35,11 +35,143 @@ function perimetroCirculo(radio) {
 }
 
 function areaCirculo(radio) {
-    return (radio * radio) * Math.PI;
+  return (radio * radio) * Math.PI;
 }
 
 console.groupEnd();
 
 
+function calcularPerimetroCuadrado() {
+  const input = document.getElementById("inputCuadrado");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro)
+}
+
+function calcularAreaCuadrado() {
+  const input = document.getElementById("inputCuadrado");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert(area)
+}
+
+function calcularPerimetroTriangulo() {
+  const input1 = document.getElementById("ladoa");
+  const value1 = parseFloat(input1.value);
+
+  const input2 = document.getElementById("ladob");
+  const value2 = parseFloat(input2.value);
+
+  const input3 = document.getElementById("base");
+  const valueBase = parseFloat(input3.value);
+
+  const input4 = document.getElementById("altura");
+  const valueAltura = parseFloat(input4.value);
 
 
+  const perimetro = perimetroTriangulo(value1, value2, valueBase);
+  alert(perimetro)
+}
+
+function calcularAreaTriangulo() {
+  const input3 = document.getElementById("base");
+  const valueBase = parseFloat(input3.value);
+
+  const input4 = document.getElementById("altura");
+  const valueAltura = parseFloat(input4.value);
+
+
+  const area = areaTriangulo(valueBase, valueAltura);
+  alert(area)
+}
+
+// circulo
+function calcularPerimetroCirculo() {
+  const input = document.getElementById("radio");
+  const value = parseFloat(input.value);
+
+  const perimetro = perimetroCirculo(value);
+  alert(perimetro)
+}
+
+function calcularAreaCirculo() {
+  const input = document.getElementById("radio");
+  const value = parseFloat(input.value);
+
+  const area = areaCirculo(value);
+  alert(area)
+}
+
+// triangulo isosceles
+function esIsosceles(a, b, c) {
+  const newArray = [];
+  const numbers = [a, b, c]
+  
+  for (let element of numbers) {
+    console.log(element)
+    if (!newArray.includes(element)) {
+      newArray.push(element);
+    }
+  }
+
+  const lenArray = newArray.length;
+  console.log(lenArray)
+  if (lenArray == 2) {
+    return true
+    // console.log("true")
+  }
+  else {
+    return false
+    // console.log("false")
+  }
+}
+
+function calcularAlturaTriangulo() {
+  const input1 = document.getElementById("lado1");
+  const input2 = document.getElementById("lado2");
+  const input3 = document.getElementById("basetriangulo");
+
+
+  if (input1 == '' || input2 == '' || input3 == '') {
+    alert("Ingrese un número")
+  }
+  else {
+    // validar si es un triangulo isosceles
+    const value1 = parseFloat(input1.value);
+    const value2 = parseFloat(input2.value);
+    const valueBase = parseFloat(input3.value);
+
+    const lista = [value1, value2, valueBase]
+    if (esIsosceles(value1, value2, valueBase)) {
+      // calcular la altura
+      lista.sort((a, b) => a - b);
+      console.log(lista)
+
+      let alturaTriangulo;
+      if (lista[0] == lista[1]) {
+        const baseNueva = lista[2]/2
+        alturaTriangulo = Math.sqrt((lista[0] * lista[1]) - (baseNueva * baseNueva))
+      }
+      else if(lista[1] == lista[2]) {
+        const baseNueva = lista[0]/2
+        alturaTriangulo = Math.sqrt((lista[1] * lista[2]) - (baseNueva * baseNueva))
+      }
+
+      alert(alturaTriangulo)
+    }
+  }
+
+
+}
+
+function isVacio(letra) {
+  if (letra === '') {
+    return true
+  }
+  else {
+    return false
+  }
+  
+}
